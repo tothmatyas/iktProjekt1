b993dc0cdaaf3113e8c370b8b42d139927047181
al144 <davidasztral2007@gmail.com>
2024-10-20 23:12:16 +0200

    finish

--- sort.py
+++ sort.py
@@ -1,3 +1,5 @@
+import random
+
 f = open("ki.txt","r", encoding="utf-8")
 lines = f.readlines()
 f.close()
@@ -6,27 +8,122 @@ arr = ''.join(lines)
 arr = arr.split(";")
 
 
-if arr[0].isdigit():
+#megnézi hogy az arr int vagy string
+arrIsInt = arr[0].isnumeric()
+
+#megnézi hogy helyes e az arr
+if arrIsInt:
+    try:
+        for i in arr:
+            int(i)
+    except:
+        print("nem mind int")
+        exit()
+
+if not arrIsInt:
+    try:
+        for i in arr:
+            int(i)
+        print("nem ind string")
+        exit()
+    except:
+        pass
+
+#megcsinálja a bubble sort-ot
+def bubbleSort(arr):
     for j in range(len(arr)):
         for i in range(len(arr)-1):
             if arr[i] > arr[i+1] :
                 arr[i], arr[i+1] = arr[i+1], arr[i]
-else:
-    for j in range(len(arr)):
-        for i in range(len(arr)-1):
-            if  len(arr[i]) > len(arr[i+1]) :
-                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return arr
 
 
-print("1: Növekvő sorrend | 2: Csökkenő sorrend")
+#megcsinalja a beszúrásos rendezést
+def insertionSort(arr):
+    while arr == arr.sort():
+        lenght = len(arr)
+        
+        for i in range(1, lenght):
+            key = arr[i]
+            j = i-1
+            while j >= 0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+        return arr
+ 
+# megcsiálja a bogosort-ot
+def bogoSort(a):
+    n = len(a)
+    while (is_sorted(a)== False):
+        shuffle(a)
+ 
+#megnezi hogy az arr sorrndbe van-e
+def is_sorted(a):
+    n = len(a)
+    for i in range(0, n-1):
+        if (a[i] > a[i+1] ):
+            return False
+    return True
+ 
+#a tömb permutációjának létrehozása
+def shuffle(a):
+    n = len(a)
+    for i in range (0,n):
+        r = random.randint(0,n-1)
+        a[i], a[r] = a[r], a[i]
+
+#dolgok hozáadásának a eljárása
+def insert(thing : str):
+    thingIsInt = thing.isnumeric()
+
+    if thingIsInt and arrIsInt:
+        arr.append(thing)
+
+    elif not thingIsInt and not arrIsInt:
+        arr.append(thing)
+    
+    else:
+        print("🤡")
+        exit()
+
+print("Milyen algoritmust használjon a program? 1: bubble sort | 2:  beszúrásos rendezés | 3: bogosort")
 inp = int(input())
-if inp == 2:
-    arr = arr[::-1]
+
+match inp:
+    case 1:
+        bubbleSort(arr)
+    case 2:
+        insertionSort(arr)
+    case 3:
+        bogoSort(arr)
+    case _:
+        print("nem helyes számot adtál meg kopo")
+
+print("akarsz hozzáadni egy új elemet 1: igen | 2: nem")
+inp = int(input())
+
+match inp:
+    case 1:
+        print("akkor adjá")
+        insert(input())
+        bubbleSort(arr)
+
+
+print("1:növekvő vagy 2:csökkenő legyen? ")
+inp = int(input())
+
+match inp:
+    case 1:
+        pass
+    case 2:
+        arr = arr[::-1]
+    case _: 
+        #print("nem helyes számot adtál meg")
+        print("🤡")
+        exit()
 
 print(arr)
-
-
-
 
 
 
